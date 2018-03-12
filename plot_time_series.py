57ceb814413a180c90f7915b92365d16d76fbd72
ragtz <agrobomaniac@gmail.com>
2018-03-12 15:42:23 -0500

    Add plot time series

---
+++ plot_time_series.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+
+def main():
+    parser = argparse.ArgumentParser(description='Plot time series data in dataset')
+    parser.add_argument('--filename', metavar='PKL', required=True, help='Dataset pickle file')
+    parser.add_argument('--state_spaces', metavar='PKL', nargs='+', required=False, default=['state', 'action'], help='Dataset pickle file')
+
+    args = parser.parse_args()
+    filename = args.filename
+    state_spaces = args.state_spaces
+
+    data = pickle.load(open(filename))
+
+    for pid in data:
+        for task in data[pid]:
+            for demo_num in data[pid][task]:
+                demo = data[pid][task][demo_num]
+                t = demo['t']
+                fig, ax = plt.subplots(len(state_spaces), 1, sharex=True)
+                fig.suptitle(pid+'_'+task+'_'+demo_num.split('_')[0])
+                for i, state_space in enumerate(state_spaces):
+                    x = demo[state_space]
+                    n = demo[state_space+'_names']
+                    ax[i].plot(t, x)
+                    ax[i].legend(n)
+                    ax[i].set_title(state_space)
+                ax[-1].set_xlabel('Time (s)')
+    plt.show()
+                    
+if __name__ == '__main__':
+    main()
+
