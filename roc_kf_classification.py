d12afa49ffabc1cd768117507b982af53799e4ff
ragtz <agrobomaniac@gmail.com>
2018-03-21 18:14:13 -0500

    Add feature evaluation via ROC script

---
+++ roc_kf_classification.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_curve, auc
+from sklearn.svm import SVC
+from keyframe_dataset import *
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import sys
+
+feature_names = {'drawer': ['table_drawer_dist', 'gripper_drawer_dist', 'gripper_state',
+                            'drawer_hist_0', 'drawer_hist_1', 'drawer_hist_2', 'drawer_color_0',
+                            'drawer_color_1', 'drawer_color_2', 'drawer_volume'],
+                 'lamp': ['table_lamp_dist', 'gripper_lamp_dist', 'gripper_state',
+                          'lamp_hist_0', 'lamp_hist_1', 'lamp_hist_2', 'lamp_color_0',
+                          'lamp_color_1', 'lamp_color_2', 'lamp_volume'],
+                 'pitcher': ['pitcher_bowl_dist', 'table_pitcher_dist', 'table_bowl_dist',
+                             'gripper_pitcher_dist', 'gripper_bowl_dist', 'gripper_state',
+                             'pitcher_hist_0', 'pitcher_hist_1', 'pitcher_hist_2', 'bowl_hist_0',
+                             'bowl_hist_1', 'bowl_hist_2', 'bowl_color_0', 'bowl_color_1',
+                             'bowl_color_2', 'pitcher_color_0', 'pitcher_color_1',
+                             'pitcher_color_2', 'bowl_volume', 'pitcher_volume'],
+                 'bowl': ['large_bowl_spoon_dist', 'large_bowl_small_bowl_dist',
+                          'spoon_small_bowl_dist', 'table_large_bowl_dist',
+                          'table_spoon_dist', 'table_small_bowl_dist',
+                          'gripper_large_bowl_dist', 'gripper_spoon_dist',
+                          'gripper_small_bowl_dist', 'gripper_state']}
+
+def main():
+    data_file = sys.argv[1]
+    task = sys.argv[2]
+
+    clf = SVC()
+
+    dataset = KeyframeDataset()
+    dataset.load(data_file)
+    dataset = dataset.get_keyframe_dataset(task=task)
+
+    X = dataset.data['kf']
+    y = dataset.data['label']
+
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
+
+    fprs = []
+    tprs = []
+    roc_aucs = []
+    for i in range(X.shape[1]):
+        x = X[:,i]
+        x = np.reshape(x, (len(x), 1))
+
+        clf.fit(x, y)
+        p = clf.decision_function(x)
+
+        fpr, tpr, _ = roc_curve(y, p)
+        roc_auc = auc(fpr, tpr)
+
+        fprs.append(fpr)
+        tprs.append(tpr)
+        roc_aucs.append(roc_auc)
+
+    cols = 5
+    rows = len(fprs)/cols
+    if len(fprs)%cols != 0:
+        rows += 1
+
+    fig, ax = plt.subplots(rows, cols, sharex='col', sharey='row')
+    fig.suptitle(task+' feature ROCs')
+    fig.text(0.5, 0.05, 'False Positive Rate', ha='center')
+    fig.text(0.08, 0.5, 'True Positive Rate', va='center', rotation='vertical')
+    for i in range(len(fprs)):
+        r = i/5
+        c = i%5
+       
+        fpr = fprs[i]
+        tpr = tprs[i]
+        roc_auc = roc_aucs[i]
+
+        ax[r,c].plot(fpr, tpr, color='darkorange', lw=2, label='area = %0.2f' % roc_auc)
+        ax[r,c].plot([0,1], [0,1], color='navy', lw=2, linestyle='--')
+        ax[r,c].set_xlim([0,1])
+        ax[r,c].set_ylim([0,1.05])
+        ax[r,c].set_title(feature_names[task][i])
+        ax[r,c].legend(loc='upper left')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
