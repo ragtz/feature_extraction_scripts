d52a95456e67ab7962562f621edcca21d1bb5205
ragtz <agrobomaniac@gmail.com>
2018-04-17 17:13:09 -0500

    Fix small bugs

--- segmentation.py
+++ segmentation.py
@@ -3,6 +3,22 @@
 from copy import deepcopy
 import numpy as np
 import argparse
+import pickle
+
+def get_pid_num(pid):
+    return pid[1:]
+
+def get_did_num(did):
+    return did.split('_')[0][1:]
+
+def sort_keys(keys, f):
+    return np.array(keys)[np.argsort([f(k) for k in keys])]
+
+def sort_pids(pids):
+    return sort_keys(pids, get_pid_num)
+
+def sort_dids(dids):
+    return sort_keys(dids, get_did_num)
 
 def handle_pids(data, pids):
     if not pids:
@@ -23,9 +39,9 @@ def data_subset(data, pids, tasks):
 
     d = {}
 
-    for pid in demos:
+    for pid in data:
         if pid in pids:
-            for task in demos[pid]:
+            for task in data[pid]:
                 if task in tasks:
                     for demo in data[pid][task]:
                         if not pid in d:
@@ -45,10 +61,14 @@ def get_segment(d, t, t_start, t_end):
 
 def change(pre, post, threshold):
     joint = np.concatenate((pre,post))
-    var_pre = np.var(pre, axis=1)
-    var_post = np.var(post, axis=1)
-    var_joint = np.var(joint, axis=1_
-    return np.max(np.concatenate((var_pre/var_joint, var_post/var_joint)), axis=1) > threshold
+    var_pre = np.var(pre, axis=0)
+    var_post = np.var(post, axis=0)
+    var_joint = np.var(joint, axis=0)
+
+    var_pre_joint_ratio = (var_pre/var_joint)[np.newaxis,:]
+    var_post_joint_ratio = (var_post/var_joint)[np.newaxis,:]
+    
+    return np.max(np.concatenate((var_pre_joint_ratio, var_post_joint_ratio)), axis=0) > threshold
 
 def segment(demo, threshold, k):
     t = demo['t']
@@ -60,8 +80,10 @@ def segment(demo, threshold, k):
         prev_kf = keyframes[idx]
         next_kf = keyframes[idx+2]
 
-        pre = get_segment(demo, t, prev_kf, kf)
-        post = get_segment(demo, t, kf, next_kf)
+        pre = get_segment(state, t, prev_kf, kf)
+        post = get_segment(state, t, kf, next_kf)
+        #pre = get_segment(state, t, kf-2, kf)
+        #post = get_segment(state, t, kf, kf+2)
 
         c = change(pre, post, threshold)
         if c.sum() > k:
@@ -73,9 +95,9 @@ def segment(demo, threshold, k):
 def print_segmentation_results(segs):
     for task in segs:
         print '==========', task, '=========='
-        for pid in segs[task]:
+        for pid in sort_pids(segs[task].keys()):
             print '----------', pid, '----------'
-            for demo in segs[task][pid]:
+            for demo in sort_dids(segs[task][pid].keys()):
                 print demo + ': ' + str(segs[task][pid][demo])
 
 def main():
@@ -87,7 +109,7 @@ def main():
     parser.add_argument('--tasks', metavar='TASK', nargs='+', default=[], required=False, help='Tasks from dataset to process')
 
     args = parser.parse_args()
-    data = args.data
+    data = pickle.load(open(args.data))
     thres = args.threshold
     k = args.k
     pids = args.pids
